c64e925f4764e4dac11d314d452b0b3a1e5168e5
mattinuorela <c3numa02@students.oamk.fi>
2024-10-12 19:26:16 +0300

    valmis

---
+++ js/functions.js
@@ -0,0 +1,75 @@
+const input = document.querySelector("input")
+const output = document.querySelector("output")
+const span = document.querySelector("span")
+
+const words = [
+    "ferrari",
+    "mclaren",
+    "red bull",
+    "toro rosso",
+    "haas",
+    "mercedes-amg",
+    "audi",
+    "alpine",
+    "aston martin",
+    "williams",
+
+]
+
+let randomizedWord = ''
+let maskedWord = ''
+let guessCount= 0
+
+const newGame = () => {
+    const random = Math.floor(Math.random() * 10) + 1
+    randomizedWord = words [random]
+    maskedWord = "*".repeat(randomizedWord.length)
+    console.log(randomizedWord)
+    output.innerHTML = maskedWord
+    guessCount = 0
+    span.innerHTML = guessCount
+}
+
+const win = () => {
+    alert(`You have guessed right, the word is ${randomizedWord}. u needed ${guessCount} guesses`)
+    newGame()
+
+}
+
+const replaceFoundChars = (guess) => {
+    for (let i = 0;i<randomizedWord.length;i++) {
+        const char = randomizedWord.substring(i,i+1)
+        if (char === guess) {
+            let newString = maskedWord.split('')
+            newString.splice(i,1,guess)
+            newString = newString.join('')
+            maskedWord = newString
+        }
+    }
+    output.innerHTML = maskedWord
+}
+
+newGame()
+
+input.addEventListener('keypress',(e) => {
+    if (e.key === 'Enter') {
+        e.preventDefault()
+
+        const guess = input.value
+        guessCount++
+        span.innerHTML = guessCount
+
+        if (guess.toLowerCase()=== randomizedWord.toLowerCase()) {
+            win()
+        }else if (guess.length === 1) {
+            replaceFoundChars(guess)
+            if (maskedWord.toLocaleLowerCase()===randomizedWord.toLocaleLowerCase()) {
+                win()
+            }
+        }else {
+            alert("Wrong guess")
+        }
+        input.value = ''
+
+    }
+})
